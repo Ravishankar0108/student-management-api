0768f23576e6ecdd847853c9cacb8a5d258fc414
Ravi Shankar Shukla <ravishankar9962@gmail.com>
2026-04-24 15:41:17 +0530

    Update StudentController.java

--- src/main/java/com/ravi/studentapi/StudentController.java
+++ src/main/java/com/ravi/studentapi/StudentController.java
@@ -1,42 +1,36 @@
-package com.ravi.studentapi;
-
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class StudentController {
-
-    private final StudentRepository studentRepository;
-
-    public StudentController(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
-
-   @GetMapping("/")
+@GetMapping("/")
 public String home() {
-    return "<h1>Welcome to Student Management by Ravi Shankar Shukla</h1>" +
-           "<a href='/students'>View Students</a>";
-}
+    return """
+    <h1>Welcome to Student Management by Ravi Shankar Shukla</h1>
+    <button onclick="addStudents()">Add Students</button>
+    <br><br>
+    <a href="/students">View Students</a>
 
-    @GetMapping("/students")
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
-    }
+    <script>
+        async function addStudents() {
+            let count = prompt("How many students do you want to add?");
 
-    @PostMapping("/students")
-    public Student createStudent(@RequestBody Student student) {
-        return studentRepository.save(student);
-    }
+            for (let i = 1; i <= count; i++) {
+                let name = prompt("Enter name of student " + i);
+                let email = prompt("Enter email of student " + i);
+                let course = prompt("Enter course of student " + i);
 
-    @DeleteMapping("/students/{id}")
-    public void deleteStudent(@PathVariable Long id) {
-        studentRepository.deleteById(id);
-    }
+                await fetch('/students', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: name,
+                        email: email,
+                        course: course
+                    })
+                });
+            }
+
+            alert("All students added successfully!");
+            window.location.href = "/students";
+        }
+    </script>
+    """;
 }
